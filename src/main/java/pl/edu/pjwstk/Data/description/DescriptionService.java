40579dbc41ee326e96622d8019bd0d232e2185bc
Kintama <kubaisnow@gmail.com>
2022-01-20 04:56:47 +0100

    Verified existing and added lacking dependencies. Added simple main controller class. (basics for testing) Added main HTML template for testing (to be refined) Added entity classes for both tables. (final) Added repository classes to handle both tables. (fully functional, TBD if final) Added test for those repository handlers. (final)

---
+++ src/main/java/pl/edu/pjwstk/Data/description/DescriptionService.java
@@ -0,0 +1,16 @@
+package pl.edu.pjwstk.Data.description;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DescriptionService {
+    @Autowired
+    private DescriptionRepository repo;
+
+    public List<Description> listAll(){
+        return (List<Description>) repo.findAll();
+    }
+}
---
+++ src/main/java/pl/edu/pjwstk/Data/recipes/RecipeService.java
@@ -0,0 +1,16 @@
+package pl.edu.pjwstk.Data.recipes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecipeService {
+    @Autowired
+    private RecipeRepository repo;
+
+    public List<Recipe> listAll(){
+        return (List<Recipe>) repo.findAll();
+    }
+}
